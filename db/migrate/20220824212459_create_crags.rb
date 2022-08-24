ebf017bda07df4bf9e0997edb5ce25970d36a3bc
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-08-24 15:35:42 -0600

    Initial commit

---
+++ db/migrate/20220824212459_create_crags.rb
@@ -0,0 +1,9 @@
+class CreateCrags < ActiveRecord::Migration[5.2]
+  def change
+    create_table :crags do |t|
+      t.string :crag_name
+      t.boolean :reservation_required
+      t.integer :elevation
+    end
+  end
+end
