516bda657f958bd00507b2fa447bd3259baed5eb
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-08-25 13:38:51 -0600

    add shoulda-matchers for testing relationships and seeds for database functionality. Update tests with before :each blocks.

--- spec/features/crags/show_spec.rb
+++ spec/features/crags/show_spec.rb
@@ -1,19 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'crags show page' do
-  it 'displays the crag name' do
-    crag = Crag.create!(crag_name: 'Redgarden Wall', reservation_required: true, elevation: 6800)
-    visit "/crags/#{crag.id}"
+  before :each do
+    @crag1 = Crag.create!(crag_name: 'Redgarden Wall', reservation_required: true, elevation: 6800)
+    @route1 = @crag1.routes.create!(route_name: 'Desdichado', sport_route: true, trad_route: false, pitches: 1, grade: "5.13d")
+    @route2 = @crag1.routes.create!(route_name: 'Yellow Spur', sport_route: false, trad_route: true, pitches: 7, grade: "5.9")
 
-    expect(page).to have_content(crag.crag_name)
+    @crag2 = Crag.create!(crag_name: 'Bell Buttress', reservation_required: false, elevation: 7200)
+    @route3 = @crag2.routes.create!(route_name: 'Verve', sport_route: true, trad_route: false, pitches: 1, grade: "5.13c")
+    @route4 = @crag2.routes.create!(route_name: 'Cosmosis', sport_route: false, trad_route: true, pitches: 3, grade: "5.9")
   end
+  
+  it 'displays the crag name' do
 
+    visit "/crags/#{@crag1.id}"
+
+    expect(page).to have_content(@crag1.crag_name)
+    expect(page).to_not have_content(@crag2.crag_name)
+  end
+  
   it 'displays the crag information' do
-    crag = Crag.create!(crag_name: 'Redgarden Wall', reservation_required: true, elevation: 6800)
-    visit "/crags/#{crag.id}"
 
-    expect(page).to have_content(crag.crag_name)
-    expect(page).to have_content(crag.reservation_required)
-    expect(page).to have_content(crag.elevation)
+    visit "/crags/#{@crag2.id}"
+
+    expect(page).to have_content(@crag2.crag_name)
+    expect(page).to have_content(@crag2.reservation_required)
+    expect(page).to have_content(@crag2.elevation)
+    expect(page).to_not have_content(@crag1.crag_name)
+    expect(page).to_not have_content(@crag1.reservation_required)
+    expect(page).to_not have_content(@crag1.elevation)
   end
 end
--- spec/features/routes/show_spec.rb
+++ spec/features/routes/show_spec.rb
@@ -1,30 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'routes show page' do
+  before :each do
+    @crag1 = Crag.create!(crag_name: 'Redgarden Wall', reservation_required: true, elevation: 6800)
+    @route1 = @crag1.routes.create!(route_name: 'Desdichado', sport_route: true, trad_route: false, pitches: 1, grade: "5.13d")
+    @route2 = @crag1.routes.create!(route_name: 'Yellow Spur', sport_route: false, trad_route: true, pitches: 7, grade: "5.9")
+
+    @crag2 = Crag.create!(crag_name: 'Bell Buttress', reservation_required: false, elevation: 7200)
+    @route3 = @crag2.routes.create!(route_name: 'Verve', sport_route: true, trad_route: false, pitches: 1, grade: "5.13c")
+    @route4 = @crag2.routes.create!(route_name: 'Cosmosis', sport_route: false, trad_route: true, pitches: 3, grade: "5.9")
+  end
+
   it 'displays the route name' do
-    crag = Crag.create!(crag_name: 'Redgarden Wall', reservation_required: true, elevation: 6800)
-    route = crag.routes.create!(route_name: 'Desdichado', sport_route: true, trad_route: false, pitches: 1, grade: "5.13d")
 
-    visit "/routes/#{route.id}"
+    visit "/routes/#{@route1.id}"
 
-    expect(page).to have_content(route.route_name)
+    expect(page).to have_content(@route1.route_name)
+    expect(page).to_not have_content(@route2.route_name)
   end
 
   it 'displays the route information' do
-    crag = Crag.create!(crag_name: 'Redgarden Wall', reservation_required: true, elevation: 6800)
-    route = crag.routes.create!(route_name: 'Desdichado', sport_route: true, trad_route: false, pitches: 1, grade: "5.13d")
 
-    visit "/routes/#{route.id}"
+    visit "/routes/#{@route3.id}"
 
-
-    expect(page).to have_content(route.route_name)
-    expect(page).to have_content(route.sport_route)
-    expect(page).to have_content(route.trad_route)
-    expect(page).to have_content(route.pitches)
-    expect(page).to have_content(route.grade)
-    expect(page).to have_content(route.created_at)
-    expect(page).to have_content(route.updated_at)
-    expect(page).to have_content(route.crag_id)
-    binding.pry
+    expect(page).to have_content(@route3.route_name)
+    expect(page).to have_content(@route3.sport_route)
+    expect(page).to have_content(@route3.trad_route)
+    expect(page).to have_content(@route3.pitches)
+    expect(page).to have_content(@route3.grade)
+    expect(page).to have_content(@route3.created_at)
+    expect(page).to have_content(@route3.updated_at)
+    expect(page).to have_content(@route3.crag_id)
+    expect(page).to_not have_content(@route1.route_name)
+    expect(page).to_not have_content(@route2.route_name)
+    expect(page).to_not have_content(@route4.route_name)
   end
 end
